58ef8fa4400c1c9ce160f14592a8b9c1c94df18b
ogataka50 <ogataka50@gmail.com>
2014-08-06 14:58:33 +0900

    add test

---
+++ tests/controllers/welcomeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+class WelcomeTest extends \CIUnit_TestCase 
+{
+	public function setUp()
+	{
+		// Set the tested controller
+		$this->CI = set_controller('welcome');
+	}
+
+	/**
+	 * @test
+	 */
+    public function hogetest()
+    {
+		// Call the controllers method
+		$this->CI->index();
+		
+		// Fetch the buffered output
+		$out = output();
+		
+		// Check if the content is OK
+		$this->assertSame(0, preg_match('/(error|notice)/i', $out));
+
+
+	 	$this->assertEquals(0, 0);
+	 	$this->assertEquals(0, 0);
+	 	$this->assertEquals(0, 0);
+	 	$this->assertEquals(0, 0);
+	 	$this->assertEquals(0, 0);
+	 	$this->assertEquals(0, 0);
+	 	$this->assertEquals(0, 0);
+	 	$this->assertEquals(0, 0);
+	}
+}
